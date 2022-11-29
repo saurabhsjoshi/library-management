7d5423cd2546128947cab82cc4963c0f4ff02f8e
Saurabh Joshi <saurabhsjoshi94@gmail.com>
2022-11-28 22:16:53 -0500

    [CODE] Integrate users API with fpts

--- user-svc/src/main/java/org/joshi/user/UserApplication.java
+++ user-svc/src/main/java/org/joshi/user/UserApplication.java
@@ -1,13 +1,20 @@
 package org.joshi.user;
 
+import com.fpts.api.filter.FilterBuilder;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @SpringBootApplication
@@ -33,5 +40,18 @@ public class UserApplication {
         repository.save(admin);
 
         log.info("Created admin user.");
+    }
+
+    @Bean
+    @Autowired
+    public FilterRegistrationBean performanceTestFilter(ApplicationContext context,
+                                                        @Qualifier("requestMappingHandlerMapping")
+                                                        RequestMappingHandlerMapping requestMappingHandler) {
+        FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
+        filterRegistration.setFilter(FilterBuilder.buildPerformanceFilter(requestMappingHandler, context));
+        List<String> urlPatterns = new ArrayList<>();
+        urlPatterns.add("/*");
+        filterRegistration.setUrlPatterns(urlPatterns);
+        return filterRegistration;
     }
 }
--- user-svc/src/main/java/org/joshi/user/UserController.java
+++ user-svc/src/main/java/org/joshi/user/UserController.java
@@ -1,12 +1,17 @@
 package org.joshi.user;
 
+import com.fpts.api.annotation.PerformanceTest;
+import com.fpts.api.enums.HttpHeaderFields;
+import com.fpts.api.enums.HttpMethodEnum;
+import com.fpts.api.model.GetTestParameter;
+import com.fpts.api.model.TestSpec;
+import com.fpts.api.model.validation.core.TestValidationsBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/users")
@@ -19,12 +24,54 @@ public class UserController {
         this.repository = repository;
     }
 
-    @GetMapping("/")
-    public List<LibraryUser> getUsers() {
-        var users = new ArrayList<LibraryUser>();
-        repository.findAll().forEach(users::add);
-        return users;
+    /**
+     * Get by username.
+     *
+     * @param username the username
+     * @return user object
+     */
+    @GetMapping("/{username}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<LibraryUser> getUser(@PathVariable String username) {
+        return repository.findById(username)
+                .map(r -> new ResponseEntity<>(r, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    /**
+     * Creates new library user.
+     *
+     * @param user user to be created
+     * @return created user
+     */
+    @PostMapping("/")
+    @ResponseStatus(HttpStatus.CREATED)
+    public LibraryUser createUser(@RequestBody LibraryUser user) {
+        return repository.save(user);
+    }
 
+    @PerformanceTest(path = "/api/users/", httpMethod = HttpMethodEnum.POST, description = "Create a library user")
+    public TestSpec<LibraryUser> postUserSpec() throws IOException {
+        var testUser = getTestUser();
+        return new TestSpec<>(testUser, new TestValidationsBuilder()
+                .addHeaderParameter(HttpHeaderFields.CREDENTIALS, "admin:admin")
+                .addHeaderParameter(HttpHeaderFields.STATUS, String.valueOf(HttpStatus.CREATED.value()))
+                .buildBodyValidationFromEntity(testUser));
+    }
+
+    @PerformanceTest(path = "/api/users/", httpMethod = HttpMethodEnum.GET, description = "Get all users")
+    public TestSpec<GetTestParameter> getUsersSpec() throws IOException {
+        GetTestParameter testParameter = new GetTestParameter("username", "testUser");
+        var testUser = getTestUser();
+        return new TestSpec<>(testParameter, new TestValidationsBuilder().buildHeaderStatus200AndEntityBody(testUser));
+    }
+
+    private static LibraryUser getTestUser() {
+        var testUser = new LibraryUser();
+        testUser.setUsername("testUser");
+        testUser.setPassword("testPassword");
+        testUser.setDisplayName("Test Name");
+
+        return testUser;
+    }
 }
