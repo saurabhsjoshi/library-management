16cacf049da69028b49ef0cfc9413f0a543637c4
Saurabh Joshi <saurabhsjoshi94@gmail.com>
2022-11-30 15:50:31 -0500

    [TEST] Implement step def to bring up and tear down the system

--- raat/src/test/java/org/joshi/raata/AcceptanceTestSuite.java
+++ raat/src/test/java/org/joshi/raata/AcceptanceTestSuite.java
@@ -2,19 +2,19 @@ package org.joshi.raata;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.*;
+import static io.cucumber.junit.platform.engine.Constants.FEATURES_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 /**
  * Test runner.
  */
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("org/joshi/raata")
+//@SelectClasspathResource("org/joshi/raata")
+//@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.joshi.raata")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.joshi.raata")
 @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources")
 public class AcceptanceTestSuite {
 }
--- raat/src/test/java/org/joshi/raata/steps/common/SystemSteps.java
+++ raat/src/test/java/org/joshi/raata/steps/common/SystemSteps.java
@@ -1,12 +1,121 @@
 package org.joshi.raata.steps.common;
 
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.model.Network;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import com.github.dockerjava.transport.DockerHttpClient;
+import io.cucumber.java.After;
 import io.cucumber.java.en.Given;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class SystemSteps {
 
+    // List of microservices
+    private final static List<String> CONTAINERS = List.of(
+            "nginx-proxy",
+            "card-svc",
+            "inventory-svc",
+            "reservation-svc",
+            "notification-svc"
+    );
+
+    private static final String NETWORK_NAME = "library-mgmt_default";
+
+    @After
+    public void teardown() {
+        stopServices();
+    }
+
     @Given("The library management system is running")
     public void systemIsRunning() {
-        System.out.println("Hello World");
+        var services = getRunningServices();
+        if (!services.isEmpty()) {
+            stopServices();
+        }
+        startServices();
+    }
+
+    private static DockerClient getDockerClient() {
+        DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
+                .build();
+
+        DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
+                .dockerHost(config.getDockerHost())
+                .sslConfig(config.getSSLConfig())
+                .maxConnections(100)
+
+                .connectionTimeout(Duration.ofSeconds(30))
+                .responseTimeout(Duration.ofSeconds(45))
+                .build();
+
+        return DockerClientImpl.getInstance(config, httpClient);
+    }
+
+    /**
+     * Method that connects to Docker daemon and retrieves running list of containers and networks corresponding to this
+     * application.
+     *
+     * @return list of active objects such as containers and networks
+     */
+    private static List<String> getRunningServices() {
+        var dockerClient = getDockerClient();
+
+        var containers = dockerClient.listContainersCmd()
+                .exec()
+                .stream()
+                .flatMap(c -> Arrays.stream(c.getNames()))
+                .filter(c -> {
+                    for (var i : CONTAINERS) {
+                        if (c.contains(i)) {
+                            return true;
+                        }
+                    }
+                    return false;
+                });
+
+        var networks = dockerClient.listNetworksCmd()
+                .exec()
+                .stream()
+                .map(Network::getName)
+                .filter(n -> n.contains(NETWORK_NAME));
+
+        try {
+            dockerClient.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return Stream.concat(containers, networks)
+                .toList();
+    }
+
+    private static void stopServices() {
+        ProcessBuilder processBuilder = new ProcessBuilder("docker-compose", "down");
+        processBuilder.directory(new File(".."));
+        try {
+            processBuilder.start().waitFor();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void startServices() {
+        ProcessBuilder processBuilder = new ProcessBuilder("docker-compose", "up", "--detach");
+        processBuilder.directory(new File(".."));
+        try {
+            processBuilder.start().waitFor();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
