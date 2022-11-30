95d9e92afe684374828c290595b4fb875a11fa13
Saurabh Joshi <saurabhsjoshi94@gmail.com>
2022-11-30 17:14:29 -0500

    [TEST] Add step def for creating user as admin

---
+++ raat/src/test/java/org/joshi/raata/steps/common/RestClient.java
@@ -0,0 +1,24 @@
+package org.joshi.raata.steps.common;
+
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.core5.http.Header;
+import org.apache.hc.core5.http.message.BasicHeader;
+
+import java.util.List;
+
+public class RestClient {
+    private static CloseableHttpClient client = null;
+
+    public static CloseableHttpClient getClient() {
+        if (client == null) {
+            client = HttpClientBuilder.create()
+                    .setDefaultHeaders(List.of(
+                            new BasicHeader("Accept", "application/json"),
+                            new BasicHeader("Content-type", "application/json")
+                    ))
+                    .build();
+        }
+        return client;
+    }
+}
--- raat/src/test/java/org/joshi/raata/steps/service/user/UserSvcStepDefs.java
+++ raat/src/test/java/org/joshi/raata/steps/service/user/UserSvcStepDefs.java
@@ -3,14 +3,36 @@ package org.joshi.raata.steps.service.user;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.core5.http.HttpStatus;
+import org.apache.hc.core5.http.io.entity.StringEntity;
+import org.joshi.raata.steps.common.RestClient;
+import org.junit.jupiter.api.Assertions;
 
+import java.io.IOException;
 import java.util.List;
 
 public class UserSvcStepDefs {
 
+    private static final String USERS_API = "http://localhost/api/users/";
+
     @When("Admin user creates a user with username {string}, name {string} and password {string}")
-    public void adminCreatesUser(String username, String name, String password) {
-        System.out.println("Hello world " + username + " " + name + " " + password);
+    public void adminCreatesUser(String username, String name, String password) throws IOException {
+        var client = RestClient.getClient();
+        var post = new HttpPost(USERS_API);
+        String createUserReq = """
+                {
+                    "username" : "%s",
+                    "password" : "%s",
+                    "displayName" : "%s"
+                }
+                """;
+        post.setEntity(new StringEntity(String.format(createUserReq, username, password, name)));
+        post.setHeader("Accept", "application/json");
+        post.setHeader("Content-type", "application/json");
+
+        var response = client.execute(post);
+        Assertions.assertEquals(HttpStatus.SC_CREATED, response.getCode());
     }
 
     @And("Admin fetches the user with username {string}")
