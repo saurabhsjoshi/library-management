da69a1c43b29e073bfb665e7126afbd6a436d65b
Saurabh Joshi <saurabhsjoshi94@gmail.com>
2022-12-11 18:20:16 -0500

    [CODE] Update reservation repository

--- reservation-svc/src/main/java/org/joshi/reservation/ReservationRepository.java
+++ reservation-svc/src/main/java/org/joshi/reservation/ReservationRepository.java
@@ -3,6 +3,9 @@ package org.joshi.reservation;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ReservationRepository extends CrudRepository<BookReservation, String> {
+    Optional<BookReservation> findByReservationIdAndUsername(String reservationId, String username);
 }
