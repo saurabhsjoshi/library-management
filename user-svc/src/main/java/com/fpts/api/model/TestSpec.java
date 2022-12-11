8a6d5825d6ab2ffe8e00c45885dadc5ec5f40dd8
Saurabh Joshi <saurabhsjoshi94@gmail.com>
2022-12-11 14:46:04 -0500

    [REFACTOR] Simplify the Test Spec class to a record

--- user-svc/src/main/java/com/fpts/api/model/TestSpec.java
+++ user-svc/src/main/java/com/fpts/api/model/TestSpec.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
 
 /**
  * This is the main class that will be returned in a method annotated with
- * {@link PerformanceTest}. <br>
+ * {@link com.fpts.api.annotation.PerformanceTest}. <br>
  * This class holds a parameter of type {@link Serializable} which will
  * represent the parameter to be used in the test request. <br>
  * This class may also contain several values to be used to validate the
@@ -17,24 +17,6 @@ import java.io.Serializable;
  * @param <T>
  * @author andre
  */
-public class TestSpec<T extends Serializable> {
-
-    private final Serializable testParameter;
-
-    private final TestValidationsBuilder validationData;
-
-    public TestSpec(Serializable testParameter, TestValidationsBuilder validationData) {
-        super();
-        this.testParameter = testParameter;
-        this.validationData = validationData;
-    }
-
-    public Serializable getTestParameter() {
-        return this.testParameter;
-    }
-
-    public TestValidationsBuilder getValidationData() {
-        return this.validationData;
-    }
+public record TestSpec<T extends Serializable>(Serializable testParameter, TestValidationsBuilder validationData) {
 
 }
